fb10d8ca09675653fc6678894b855051a40b7471
Berthony D <philsony77@gmail.com>
2018-02-21 18:05:25 +0800

    Restricted access for all non-login users in all pages, remove old sd-connect files

--- core.php
+++ core.php
@@ -1,9 +1,9 @@
 <?php
   require('../connect.php');
-	
+
 	define("HOMEPAGE", "location:../login/welcome.php");
 	define("CREATE_SOURCE","location:../route/create_src.php");
 
 
 	error_reporting(0);
-?>
+?> 
