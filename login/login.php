901c258a28a4d31098846d2595aed14aee973d94
Foolish656 <leofrost343@gmail.com>
2018-02-23 00:16:30 +0800

    Changed username prompt to email prompt
    Forgot to change the form

--- login/login.php
+++ login/login.php
@@ -63,7 +63,7 @@
             <div style = "margin:30px">
 
                <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Email  :</label><input type = "text" name = "email" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
