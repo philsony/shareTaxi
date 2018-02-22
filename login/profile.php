039243c3a28b20038c79858b971f899e3ee38f51
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-02-23 03:38:41 +0800

    Repush

---
+++ login/profile.php
@@ -0,0 +1,22 @@
+<?php
+  require('../connect.php');
+
+?>
+<html>
+
+   <head>
+      <title>Welcome </title>
+		<link rel="stylesheet" href="css/general_style.css" />
+      
+   </head>
+
+   <body>
+      <h1>Welcome <?php echo $loginSession; ?></h1>
+
+
+        <h2><a href = "settings.php">Settings</a></h2>
+          <h2><a href = "<?php echo BASE_URL ; ?>/market/myactive_pools.php">Sign Out</a></h2>
+   </body>
+
+
+</html>
---
+++ route/update.php
@@ -0,0 +1,24 @@
+<?php
+  require('../connect.php');
+
+	$srcAddr = $_POST['source'];
+	$srcLat	= floatval($_POST['srcLat']);
+	$srcLong = floatval($_POST['srcLong']);
+	$destAddr = $_POST['destination'];
+	$destLat = floatval($_POST['destLat']);
+	$destLong = floatval($_POST['destLong']);
+	$userID = $_POST['ID'];
+  $return = 'Location: '.BASE_URL.'login/welcome.php?action=updated&entity=Route';
+
+	// Look for userID in pool
+	$query = "SELECT * FROM pool WHERE user_id = $userID";
+	$result = mysqli_query($db, $query) or die("Error $query");
+	// Get route_id
+	$row = mysqli_fetch_assoc($result);
+	$routeId = $row['route_id'];
+	// Update route based on route_id
+	$query = "UPDATE route SET origin_address = '$srcAddr', origin_latitude= $srcLat, origin_longitude= $srcLong, destination_address = '$destAddr', destination_latitude=$destLat, destination_longitude=$destLong, status='Waiting', cost=0  WHERE route_id = $routeId";
+	$result = mysqli_query($db, $query) or die("Error $query");
+
+	header($return);
+?>
