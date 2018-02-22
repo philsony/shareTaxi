25fa61804642f79eaafc1617bd7a8ef7c4360206
philsony77@gmail.com <admin@vps162337.vps.ovh.ca>
2018-02-22 04:47:05 -1000

    Messaging system update

--- core.php
+++ core.php
@@ -2,7 +2,7 @@
 
 	define("HOMEPAGE", "location:../login/welcome.php");
 	define("CREATE_SOURCE","location:../route/create_src.php");
-  define("BASE_URL","http://".$_SERVER['HTTP_HOST']."/shareTaxi/");
+  define("BASE_URL","https://".$_SERVER['HTTP_HOST']."/");
 
 	error_reporting(0);
 ?>
