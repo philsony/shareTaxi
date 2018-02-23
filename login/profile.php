8a277a41f2b0490ef86eee5207566d84a2515310
Berthony D <philsony77@gmail.com>
2018-02-23 12:05:50 +0800

    Resolving conflicts 3

--- login/profile.php
+++ login/profile.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   require('../connect.php');
 
@@ -18,26 +17,5 @@
         <h2><a href = "settings.php">Settings</a></h2>
           <h2><a href = "<?php echo BASE_URL ; ?>/market/myactive_pools.php">Sign Out</a></h2>
    </body>
-
-=======
-<?php
-  require('../connect.php');
-
-?>
-<html>
-
-   <head>
-      <title>Welcome </title>
-   </head>
-
-   <body>
-      <h1>Welcome <?php echo $loginSession; ?></h1>
-
-
-        <h2><a href = "settings.php">Settings</a></h2>
-          <h2><a href = "<?php echo BASE_URL ; ?>/market/myactive_pools.php">Sign Out</a></h2>
-   </body>
-
->>>>>>> da52d3f8d44faff150c7da6e726997a343f31182
 
 </html>
