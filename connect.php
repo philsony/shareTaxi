7a590e63313111ff604f54c6fa63dbc0a70d5323
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-02-23 15:32:41 +0800

    Added login styling and some minor fixes.

--- connect.php
+++ connect.php
@@ -12,7 +12,7 @@ $db = mysqli_connect("144.217.5.247", $user, $pass, $db) or die("Failed to Load"
 $conn = $db ;
 $userLocation = getUserLocation();
 
-if(strpos($_SERVER["REQUEST_URI"],"updateLocation") === false){
+//if(strpos($_SERVER["REQUEST_URI"],"updateLocation") === false){
+//<!-- a href="<?php //echo BASE_URL ; ?>login/welcome.php"><h2 style="text-align: center;">Menu</h2></a -->
+
 ?>
-<a href="<?php echo BASE_URL ; ?>login/welcome.php"><h2 style="text-align: center;">Menu</h2></a>
-<?php }
