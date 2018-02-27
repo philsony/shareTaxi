a017a7be3d596910dd97de043fc65ec72433bbda
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-02-27 23:58:09 +0800

    Finalized fonts

--- core.php
+++ core.php
@@ -5,5 +5,6 @@
   define("BASE_URL","http://".$_SERVER['HTTP_HOST']."/");
   define("LOGIN_URL","Location: http://".$_SERVER['HTTP_HOST']."/login/login.php");
 	define("WELCOME_URL","Location: http://".$_SERVER['HTTP_HOST']."/login/welcome.php");
+	
 	error_reporting(0);
 ?>
