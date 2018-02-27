a90f6f6a734a81709c9772aceb13431c931b17c2
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-02-28 02:12:50 +0800

    Fixed search

--- market/search.php
+++ market/search.php
@@ -83,16 +83,21 @@
 
           // searches the database check read me for explanation
           //23 feb : Used the input string instead of the long/lat.
-          $q = "SELECT t1.*, t2.* FROM
-                (SELECT p.pool_id as `pool_id`, r.origin_latitude as `route_origlat`, r.origin_longitude as `route_origlong`, r.destination_latitude as `route_destlat`, r.destination_longitude as `route_destlong`,
-                r.route_id as `route_id`, r.cost as `route_cost`, r.status as `route_status`, r.origin_address as `add_orig`, r.destination_address as `add_dest`
-                FROM route r, pool p, users u
-                WHERE p.user_id = u.user_id AND p.route_id = r.route_id AND r.status = 'Waiting' AND u.user_id != {$_SESSION['id']} ) t1
-                LEFT JOIN
-                (SELECT route_id, COUNT(*) as num_users FROM pool GROUP BY route_id) t2
-                ON t1.route_id = t2.route_id
-                WHERE t2.num_users < 4 AND t1.add_dest LIKE '%".$_POST['latlng']."%' 
-                GROUP BY t2.route_id DESC";
+          $q = "SELECT big1.*, big2.* FROM
+                (SELECT t1.*, t2.* FROM
+                  (SELECT p.pool_id as `pool_id`, r.origin_latitude as `route_origlat`, r.origin_longitude as `route_origlong`, r.destination_latitude as `route_destlat`, r.destination_longitude as `route_destlong`,
+                  r.route_id as `route_id`, r.cost as `route_cost`, r.status as `route_status`, r.origin_address as `add_orig`, r.destination_address as `add_dest`
+                  FROM route r, pool p, users u
+                  WHERE p.user_id = u.user_id AND p.route_id = r.route_id AND r.status = 'Waiting' AND u.user_id != {$_SESSION['id']} ) t1
+                  LEFT JOIN
+                  (SELECT route_id, COUNT(*) as num_users FROM pool GROUP BY route_id) t2
+                  ON t1.route_id = t2.route_id
+                  WHERE t2.num_users < 4 AND t1.add_dest LIKE '%".$_POST['latlng']."%'
+                  GROUP BY t2.route_id DESC) big1
+                  INNER JOIN
+                  (SELECT route_id FROM `pool` WHERE user_id = {$_SESSION['id']}) big2
+                   ON big1.route_id != big2.route_id
+                    GROUP BY big1.route_id";
           
    
           $result = mysqli_query($conn, $q);
