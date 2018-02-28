c0e5c7b6fc3b9bbe356dc86aaf3a18be4b7c216f
philsony77@gmail.com <admin@vps162337.vps.ovh.ca>
2018-02-27 15:26:37 -1000

    ROute creator cannot leave it s own pool

--- market/leavepool.php
+++ market/leavepool.php
@@ -3,6 +3,13 @@
   include('../core/alerts.php');
 
   if(isset($_POST['route_id'])){
+    
+    
+	$sql="SELECT * FROM route WHERE user_id=$userId AND route_id = {$_POST['route_id']} ";
+    $result=mysqli_query($db,$sql);
+    
+	$count=mysqli_num_rows($result);
+    if($count == 0){
   	$q = "DELETE FROM pool WHERE route_id = {$_POST['route_id']} AND user_id = {$_SESSION['id']}";
   	$result = mysqli_query($conn,$q);
   	if(mysqli_affected_rows($conn)> 0){
@@ -10,5 +17,8 @@
   	}else{
   		echo "error in deleting";
   	}
+    } else {
+      echo "You cannot leave your own pool";
+    }
   }  
 ?>
