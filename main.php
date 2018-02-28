f35283a82a5b6b1ab4ea161105fc19f2a7059011
allenazamoras <allenazamoras@gmail.com>
2018-02-28 08:58:46 +0800

    Lineawesome fix

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="<?php echo BASE_URL ?>/assets/css/navigation.css" />
-<link rel="stylesheet" href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css">
+<link rel="stylesheet" href="<?php echo BASE_URL ?>/assets/css/line-awesome.css">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <div class="navigation">
