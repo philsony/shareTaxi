66c3350b72a498f3f94258b3f94159373a18c7ac
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-03-10 03:41:41 +0800

    Fixed routing

--- core.php
+++ core.php
@@ -2,9 +2,9 @@
 
 	define("HOMEPAGE", "location:../login/welcome.php");
 	define("CREATE_SOURCE","location:../route/create_src.php");
-  define("BASE_URL","http://".$_SERVER['HTTP_HOST']."/");
-  define("LOGIN_URL","Location: http://".$_SERVER['HTTP_HOST']."/login/login.php");
-	define("WELCOME_URL","Location: http://".$_SERVER['HTTP_HOST']."/login/welcome.php");
+  define("BASE_URL","https://".$_SERVER['HTTP_HOST']."/");
+  define("LOGIN_URL","Location: https://".$_SERVER['HTTP_HOST']."/login/login.php");
+	define("WELCOME_URL","Location: https://".$_SERVER['HTTP_HOST']."/login/welcome.php");
 	
 	error_reporting(0);
 ?>
