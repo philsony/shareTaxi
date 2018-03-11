0e78957ee1c485a4aef414e73832e014ae829e90
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-03-11 13:26:42 +0800

    Fixed messaging

--- messaging/index.php
+++ messaging/index.php
@@ -3,4 +3,5 @@
     require("../functions.php");
     require("../loginChecker.php");
 ?>
-<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000"><link rel="manifest" href="/messaging/manifest.json"><link rel="shortcut icon" href="/messaging/favicon.ico"><title>Message - ShareTaxi</title><link href="/messaging/static/css/main.261afba3.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script type="text/javascript" src="/messaging/static/js/main.27fbe7e7.js"></script></body></html>
+
+<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"><meta name="theme-color" content="#000000"><link rel="manifest" href="/messaging/manifest.json"><link rel="shortcut icon" href="/messaging/favicon.ico"><title>Message - ShareTaxi</title><link href="/messaging/static/css/main.261afba3.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script type="text/javascript" src="/messaging/static/js/main.4c7cfeb4.js"></script></body></html>
