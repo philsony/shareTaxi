462722d6e60be15e696c7ec2883339b73a4d2ac5
Jack Laurence Garay <lawrencetecho@gmail.com>
2018-03-12 17:39:00 +0800

    Messaging fixed.

--- route/create.php
+++ route/create.php
@@ -52,9 +52,52 @@
 		$result = mysqli_query($db, $query) or die("Error $query");
 
 		//echo "Successful";
-
-		// Return to homepage
-		// header('location:homescreen.php');
-		//Return to homepage
-		header(HOMEPAGE.'?action=created&entity=route');
 	}
+?>
+
+
+<script src="https://www.gstatic.com/firebasejs/4.11.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.10.1/firebase-firestore.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBs7VM1SUqWyLZVDSuFazk3LcSfIerp8GM",
+    authDomain: "sharetaxi-78289.firebaseapp.com",
+    databaseURL: "https://sharetaxi-78289.firebaseio.com",
+    projectId: "sharetaxi-78289",
+    storageBucket: "sharetaxi-78289.appspot.com",
+    messagingSenderId: "599684388499"
+  };
+  firebase.initializeApp(config);
+
+  var db = firebase.firestore();
+
+  db.collection("messages").doc("<?php echo $_POST['route_id']; ?>").set({
+    users: <?php echo $fs_data; ?>
+  }, {merge: true}).then(function() {
+    window.location.href = "<?php echo HOMEPAGE.'?action=created&entity=route'; ?>";
+    console.log('done');
+  });
+</script>
+
+<style>
+  html, body {
+    background-color: #292c35 !important;
+  }
+
+  .content {
+    position: fixed;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+  }
+</style>
+
+<head>
+  <meta http-equiv='refresh' content='3;<?php echo BASE_URL.'market/myactive_pools.php?action=joined&entity=You'?>');
+</head>
+
+<div class='content'>
+  <img style='width: 50px;' src='<?php echo BASE_URL ?>/assets/images/please_wait.gif' />
+</div>
